b180cf8071f7fa1b040a3adc9c67ce2bb644e24b
Lijun <gan.lijun.glj@gmail.com>
2021-02-14 18:32:23 -0500

    error fixed

--- W8D1/polymorphism/app/models/toy.rb
+++ W8D1/polymorphism/app/models/toy.rb
@@ -1,6 +1,6 @@
 class Toy < ApplicationRecord
     validates :name, :toyable_id,:toyable_type, presence:true, uniqueness:true
     belongs_to :toyable, polymorphic: true
-    validates :name, uniqueness: { scope: :toyable_id}
+    validates :name, uniqueness: { scope: :toyable}
 
 end

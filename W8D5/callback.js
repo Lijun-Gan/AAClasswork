b0b0b46008f705f21d48fa05ff6b602c645e4539
Lijun <gan.lijun.glj@gmail.com>
2021-02-20 12:19:53 -0500

    commit message"

--- W8D5/callback.js
+++ W8D5/callback.js
@@ -1,46 +1,57 @@
-// window.setTimeout(function (){
-//     alert('HAMMERTIME');
-// },5000)
-
-// function hammerTime(time){
-//     console.log("its running")
-//     window.setTimeout(function(){
-//         alert(`${time} is hammertime!`)
-//     },3000)
-// }
-
-// hammerTime(W8D45000)
-
-
 const readline = require('readline');
 
 const reader = readline.createInterface({
-  // it's okay if this part is magic; it just says that we want to
-  // 1. output the prompt to the standard output (console)
-  // 2. read input from the standard input (again, console)
 
   input: process.stdin,
   output: process.stdout
 });
 
-function teaAndBiscuits () {
-    let first, second;
-  
-    reader.question('Would you like some tea?', (res) => {
-      first = res;
+// function teaAndBiscuits(){
+//   reader.question('Would you like some tea?',function(res){
+//     console.log(`You replied ${res}.`);
+//     reader.question('Would you like some biscuits?',function(res2){
+//       console.log(`You replied ${res2}.`);
+//       const first=(res==='yes')?'do':'don\'t';
+//       const second=(res2==='yes')?'do':'don\'t';
+//       console.log(`So you ${first} want tea and you ${second} want biscuits.`);
+//       reader.close();
+//     });
+//   });
+// }
+
+function teaAndBiscuits(){
+    reader.question('Would you like some tea?',function(res){
       console.log(`You replied ${res}.`);
+      reader.question('Would you like some biscuits?',function(res2){
+        console.log(`You replied ${res2}.`);
+
+        console.log(`So you ${res} want tea and you ${res2
+        } want biscuits.`);
+        reader.close();
+      });
     });
-  
-    reader.question('Would you like some biscuits?', (res) => {
-      second = res;
-      console.log(`You replied ${res}.`);
-    });
-  
-    const firstRes = (first === 'yes') ? 'do' : 'don\'t';
-    const secondRes = (second === 'yes') ? 'do' : 'don\'t';
-    console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
-    reader.close();
   }
 
-  teaAndBiscuits();
+teaAndBiscuits()
+
+// function teaAndBiscuits () {
+//     let first, second;
+  
+//     reader.question('Would you like some tea?', (res) => {
+//       first = res;
+//       console.log(`You replied ${res}.`);
+//     });
+  
+//     reader.question('Would you like some biscuits?', (res) => {
+//       second = res;
+//       console.log(`You replied ${res}.`);
+//     });
+  
+//     const firstRes = (first === 'yes') ? 'do' : 'don\'t';
+//     const secondRes = (second === 'yes') ? 'do' : 'don\'t';
+//     console.log(`So you ${firstRes} want tea and you ${secondRes} want biscuits.`);
+//     reader.close();
+//   }
+
+//   teaAndBiscuits();
 
---
+++ W8D5/timeout.js
@@ -0,0 +1,15 @@
+window.setTimeout(function (){
+    alert('HAMMERTIME');
+},5000)
+
+function hammerTime(time){
+
+    window.setTimeout(function(){
+        alert(`${time} is hammertime!`)
+    },time)
+}
+
+hammerTime(5000)
+
+
+

0f41dd62344d56c9cd55767095cefd8a34865010
Lijun <gan.lijun.glj@gmail.com>
2021-03-07 17:17:20 -0500

    w11d1

---
+++ W11D1/frontend/store/store.js
@@ -0,0 +1,52 @@
+import { createStore,applyMiddleware } from 'redux';
+import rootReducer from '../reducers/root_reducer';
+
+const configureStore = (preloadedState = {}) => {
+  const store = createStore(
+    rootReducer, 
+    preloadedState,
+    // Phase 3: Using Redux applyMiddleware
+    applyMiddleware(addLoggingToDispatch,testingMiddleware)
+    // // Phase 3***:
+    );
+ 
+  store.subscribe(() => {
+    localStorage.state = JSON.stringify(store.getState());
+  });
+
+  return store;
+}
+
+  // ***// Phase 3: Using Redux applyMiddleware
+
+// const addLoggingToDispatch = store => next => action => {
+//   console.log('Action received:', action);
+//   console.log('State pre-dispatch:', store.getState());
+//   console.log('next: ', next)
+//   let result = next(action);
+//   console.log('next(action): ',result)
+//   console.log('State post-dispatch:', store.getState());
+//   return result
+// }
+
+////question4, hwo to get the store from line 9?
+/// why do we need return result on phase 2? not phase 3?
+// check screenshot questions
+
+
+const addLoggingToDispatch = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  next(action);
+  console.log(store.getState());
+};
+ // // Phase 3***:
+
+
+ const testingMiddleware = store => next => action => {
+  console.log("testing");
+  next(action);
+};
+
+export default configureStore;
+
---
+++ W11D1/frontend/todo_redux.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+    let store = configureStore(preloadedState);
+    
+//   // ***phase 1
+//   store.dispatch = addLoggingToDispatch(store)
+//  // phase 1****
+
+//   // ***phase 2
+// store = applyMiddlewares(store,addLoggingToDispatch)
+//  // phase 2****
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+
+  window.store = store;
+});
+
+//   // ***phase 2
+// const addLoggingToDispatch = store => next => action => {
+//   console.log('Action received:', action);
+//   console.log('State pre-dispatch:', store.getState());
+//   console.log('next: ', next)
+//   let result = next(action);
+//   console.log('next(action): ',result)
+//   console.log('State post-dispatch:', store.getState());
+//   return result
+// }
+
+// const applyMiddlewares = (store,...middlewares)=>{
+//   let dispatch = store.dispatch;
+//   middlewares.forEach( middleware =>{
+//     dispatch = middleware(store)(dispatch)
+//   })
+
+// //question1 what's difference between overwrite 
+// // store.dispatch vs store = Object.assign({},store,{dispatch})
+
+//   return Object.assign({},store,{dispatch})
+// } 
+
+//  // phase 2****
+
+// //***phase 1
+// const addLoggingToDispatch = (store)=>{
+//   const dispatch = store.dispatch;
+//   return (action) =>{
+//     console.log(store.getState());
+//     console.log(action);
+//     dispatch(action);
+//     console.log(store.getState())
+//   }
+// }
+//  //phase 1***
+
